8b413f2d66685eff0cb6ba1c56101426230e85f4
Fernando Augusto Haeser <fernandohaeserr@gmail.com>
2025-08-14 21:58:11 -0300

    Create ImparOuPar.c

---
+++ Linguagem da Programacao - I/ExerciciosC_Aula02/ImparOuPar.c
@@ -0,0 +1,20 @@
+/*
+* Faça um programa que leia um nº inteiro e mostre uma mensagem indicando se 
+* este número é par ou ímpar, e se é positivo ou negativo. Ex: 'O número 2 é
+* par e é positivo'.
+*/
+
+#include <stdio.h>
+
+int main() {
+    
+    int num;
+    
+    printf("Informe um numero para saber se ele é par ou ímpar: ");
+    scanf("%d", &num);
+    if (num % 2 == 0)
+        printf("\nSeu número é par!");
+    else
+        printf("\nSeu número é ímpar!");
+        
+}
