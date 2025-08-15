ebd4ad6d9a4e19cd420048120527ea5d0822b9b4
Fernando Augusto Haeser <fernandohaeserr@gmail.com>
2025-08-14 23:00:24 -0300

    Update Media&Aprovacao.c

--- Linguagem da Programacao - I/ExerciciosC_Aula02/Media&Aprovacao.c
+++ Linguagem da Programacao - I/ExerciciosC_Aula02/Media&Aprovacao.c
@@ -1,49 +1,39 @@
-/*
-* Leia 03 (três) notas de um aluno, calcule sua média. 
-* Caso a média seja menor que 7 ele será reprovado.
-* Apresente a média calculada e a situação (aprovado ou reprovado).
-*/
-
 #include <stdio.h>
 
 int main()
 {
     int nota1, nota2, nota3;
     float media;
-    _Bool continuar = 0;
     
+    // Nota 1
     do {
-        
-    printf("\nInforme sua primeira nota: ");
-    scanf("%d", &nota1);
-    
-    if (nota1 > 10 || nota1 < 0) {
-        printf("\nInforme uma nota valida por favor!");
-        continue;
-    }
+        printf("\nInforme sua primeira nota: ");
+        scanf("%d", &nota1);
+        if (nota1 < 0 || nota1 > 10)
+            printf("Nota inválida! Digite um valor entre 0 e 10.\n");
+    } while (nota1 < 0 || nota1 > 10);
 
-    printf("\nInforme sua segunda nota: ");
-    scanf("%d", &nota2);
+    // Nota 2
+    do {
+        printf("\nInforme sua segunda nota: ");
+        scanf("%d", &nota2);
+        if (nota2 < 0 || nota2 > 10)
+            printf("Nota inválida! Digite um valor entre 0 e 10.\n");
+    } while (nota2 < 0 || nota2 > 10);
+
+    // Nota 3
+    do {
+        printf("\nInforme sua terceira nota: ");
+        scanf("%d", &nota3);
+        if (nota3 < 0 || nota3 > 10)
+            printf("Nota inválida! Digite um valor entre 0 e 10.\n");
+    } while (nota3 < 0 || nota3 > 10);
+
+    // Corrigindo para divisão real
+    media = (nota1 + nota2 + nota3) / 3.0;
     
-    if (nota2 > 10 || nota2 < 0) {
-        printf("\nInforme uma nota valida por favor!");
-        continue;
-    }
-    
-    printf("\nInforme sua terceira nota: ");
-    scanf("%d", &nota3);
-    
-    if (nota3 > 10 || nota3 < 0) {
-        printf("\nInforme uma nota valida por favor!");
-        continue;
-    }
-    
-    continuar = 1;
-    
-    } while (!continuar);
-    
-    media = (nota1 + nota2 + nota3) / 3;
-    
+    printf("\nMédia final: %.2f", media);
+
     if (media < 7)
         printf("\nVocê foi reprovado!");
     else
