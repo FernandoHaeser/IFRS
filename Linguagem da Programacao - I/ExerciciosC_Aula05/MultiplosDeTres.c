b2f897d891b862a89c00dcea1ffe2c482866a204
Fernando Augusto Haeser <fernandohaeserr@gmail.com>
2025-09-16 20:14:53 -0300

    Create MultiplosDeTres.c

---
+++ Linguagem da Programacao - I/ExerciciosC_Aula05/MultiplosDeTres.c
@@ -0,0 +1,21 @@
+/*
+Múltiplos de 3 com WHILE
+Escreva um programa que imprima os múltiplos de 3 entre 1 e 20, em ordem crescente, utilizando o laço while.
+Dica: o primeiro múltiplo de 3 dentro do intervalo é o número 3.
+*/
+
+#include <stdio.h>
+
+int main() {
+
+  int i = 1;
+
+  while ( i <= 20) {
+    
+     if ( i % 3 == 0 || i % 6 == 0 || i & 9 == 0)
+       printf("%d \n", i);
+    
+    i++;
+    
+  }
+}
