4ddae64611794877000c677dc83cd29186e1f85b
Fernando Augusto Haeser <fernandohaeserr@gmail.com>
2025-09-16 20:27:04 -0300

    Change output order to ascending for multiples of 3
    Altered the program to print multiples of 3 in ascending order using a do-while loop instead of a while loop.

--- Linguagem da Programacao - I/ExerciciosC_Aula05/MultiplosTresDecrescente.c
+++ Linguagem da Programacao - I/ExerciciosC_Aula05/MultiplosTresDecrescente.c
@@ -1,6 +1,6 @@
 /*
 Múltiplos de 3 com WHILE
-Escreva um programa que imprima os múltiplos de 3 entre 1 e 20, em ordem decrescente, utilizando o laço while.
+Escreva um programa que imprima os múltiplos de 3 entre 1 e 20, em ordem crescente, utilizando o laço while.
 Dica: o primeiro múltiplo de 3 dentro do intervalo é o número 3.
 */
 
@@ -9,16 +9,16 @@ Dica: o primeiro múltiplo de 3 dentro do intervalo é o número 3.
 int main() {
 
   int i = 20;
-
-  while ( i >= 0) {
+  
+  do {
       
-     if (i == 0)
+    if (i == 0)
         continue;
     
-     if ( i % 3 == 0 || i % 6 == 0 || i & 9 == 0)
+    if ( i % 3 == 0 || i % 6 == 0 || i & 9 == 0)
        printf("%d \n", i);
     
     i--;
-    
-  }
+  
+    } while ( i >= 0);
 }
