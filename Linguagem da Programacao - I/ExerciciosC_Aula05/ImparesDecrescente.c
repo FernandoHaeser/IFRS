34cda95940a594469ce3aae1e2a0d84c810039f7
Fernando Augusto Haeser <fernandohaeserr@gmail.com>
2025-09-16 19:56:00 -0300

    Create ImparesDecrescente.c

---
+++ Linguagem da Programacao - I/ExerciciosC_Aula05/ImparesDecrescente.c
@@ -0,0 +1,15 @@
+/*
+Ímpares com FOR em ordem decrescente
+Escreva um programa que imprima os números ímpares entre 1 e 20, em ordem decrescente, utilizando o comando for.
+*/
+
+#include <stdio.h>
+
+int main() {
+
+  for (int i = 20; i >= 0; i--) {
+
+      if( i % 2 == 1)
+        printf("%d \n", i);
+  }
+}
