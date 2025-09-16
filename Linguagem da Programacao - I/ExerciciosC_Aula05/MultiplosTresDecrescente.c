878b1d6ebb3e0ddc6e3ac40119bf70d7a3377f4e
Fernando Augusto Haeser <fernandohaeserr@gmail.com>
2025-09-16 20:17:32 -0300

    Add program to print multiples of 3 in descending order
    Implement program to print multiples of 3 from 20 to 1 using while loop.

---
+++ Linguagem da Programacao - I/ExerciciosC_Aula05/MultiplosTresDecrescente.c
@@ -0,0 +1,24 @@
+/*
+Múltiplos de 3 com WHILE
+Escreva um programa que imprima os múltiplos de 3 entre 1 e 20, em ordem decrescente, utilizando o laço while.
+Dica: o primeiro múltiplo de 3 dentro do intervalo é o número 3.
+*/
+
+#include <stdio.h>
+
+int main() {
+
+  int i = 20;
+
+  while ( i >= 0) {
+      
+     if (i == 0)
+        continue;
+    
+     if ( i % 3 == 0 || i % 6 == 0 || i & 9 == 0)
+       printf("%d \n", i);
+    
+    i--;
+    
+  }
+}
