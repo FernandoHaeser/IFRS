1495040b052d7d49aed19f6b953be0422af0dfac
Fernando A. Haeser <fernandohaeserr@gmail.com>
2025-09-16 20:34:58 -0300

    adicionando por git

--- Linguagem da Programacao - I/ExerciciosC_Aula05/ContagemDecremento.c
+++ Linguagem da Programacao - I/ExerciciosC_Aula05/ContagemDecremento.c
@@ -6,4 +6,10 @@ Escreva um programa que imprima os números de 1 a 10 na tela, em ordem crescent
 #include <stdio.h>
 
 int main() {
+
+    for (int i = 10; i > 0; i--) {
+        
+        printf("%d \n", i);
+        
+    }
   
