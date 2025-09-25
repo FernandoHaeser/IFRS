624e2aaaf3d32249663b4137e51d611c0a76f00c
Fernando A. Haeser <fernandohaeserr@gmail.com>
2025-09-24 22:25:55 -0300

    primeira versao urna

--- Linguagem da Programacao - I/Simulado/Exercicio3.c
+++ Linguagem da Programacao - I/Simulado/Exercicio3.c
@@ -40,5 +40,84 @@ Ao FINAL deve ser exibido o nome do programador.
 #include <stdio.h>
 
 int main() {
-    
+
+    int voto_vereador, voto_prefeito, total_nulos_vereador = 0, total_nulos_prefeito = 0, total_brancos_vereador = 0, total_brancos_prefeito = 0;
+    int votos_candidato_vereadorA, votos_candidato_vereadorB, votos_candidato_vereadorC, votos_candidato_vereadorD, votos_candidato_prefeitoX, votos_candidato_prefeitoY, votos_candidato_prefeitoZ;
+
+    for (int i = 0; i < 7; i++) {
+        
+        printf("Informe seu voto para Vereador\n\n");
+        printf("-> 66666 para candidato A\n"); 
+        printf("-> 77777 para candidato B\n");
+        printf("-> 88888 para candidato C\n"); 
+        printf("-> 99999 para candidato D\n");
+        printf("-> 111 para candidato BRANCO\n");
+        printf("-> QUALQUER NUMERO para candidato NULO\n");
+        printf("R: ");
+        scanf("%d", &voto_vereador);
+
+        switch (voto_vereador) {
+            case 66666: 
+                votos_candidato_vereadorA++;
+                break;
+            case 77777: 
+                votos_candidato_vereadorB++;
+                break;
+            case 88888: 
+                votos_candidato_vereadorC++;
+                break;
+            case 99999: 
+                votos_candidato_vereadorD++;
+                break;
+            case 111:
+                total_brancos_vereador++;
+                break;
+            default:
+                total_nulos_vereador++; 
+        }
+
+        printf("Informe seu voto para Prefeito\n\n");
+        printf("-> 99 para candidato X\n"); 
+        printf("-> 88 para candidato Y\n");
+        printf("-> 77 para candidato Z\n"); 
+        printf("-> 111 para candidato BRANCO\n");
+        printf("-> QUALQUER NUMERO para candidato NULO\n");
+        printf("R: ");
+        scanf("%d", &voto_prefeito);
+
+        switch (voto_prefeito) {
+            case 99: 
+                votos_candidato_prefeitoX++;
+                break;
+            case 88: 
+                votos_candidato_prefeitoY++;
+                break;
+            case 77: 
+                votos_candidato_prefeitoZ++;
+                break;
+            case 111:
+                total_brancos_prefeito++;
+                break;
+            default:
+                total_nulos_prefeito++; 
+        }
+
+        int total_votos_vereador = votos_candidato_vereadorA + votos_candidato_vereadorB + votos_candidato_vereadorC + votos_candidato_vereadorD;
+
+        printf("VOTOS PARA VEREADOR\n\n");
+        printf("Vereador A: %d \n", votos_candidato_vereadorA);
+        printf("Vereador A: %d \n", votos_candidato_vereadorB);
+        printf("Vereador A: %d \n", votos_candidato_vereadorC);
+        printf("Vereador A: %d \n", votos_candidato_vereadorD);
+        printf("Vereador A: %d \n", total_nulos_vereador);
+        printf("Vereador A: %d \n\n\n", total_brancos_vereador);
+
+        printf("VOTOS PARA VEREADOR\n\n");
+        printf("Vereador A: %d \n", votos_candidato_vereadorA);
+        printf("Vereador A: %d \n", votos_candidato_vereadorB);
+        printf("Vereador A: %d \n", votos_candidato_vereadorC);
+        printf("Vereador A: %d \n", votos_candidato_vereadorD);
+        printf("Vereador A: %d \n", total_nulos_vereador);
+        printf("Vereador A: %d \n", total_brancos_vereador);
+    }
 }
