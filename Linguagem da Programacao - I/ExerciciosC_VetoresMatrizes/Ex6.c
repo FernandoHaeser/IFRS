a086e3fdb2729b908fc8e725046887e3b1c78afc
FernandoHaeser <fernandohaeserr@gmail.com>
2025-10-29 08:50:51 -0300

    :zap:Exercicio-6: Resolvido

--- Linguagem da Programacao - I/ExerciciosC_VetoresMatrizes/Ex6.c
+++ Linguagem da Programacao - I/ExerciciosC_VetoresMatrizes/Ex6.c
@@ -1,7 +1,7 @@
 /*
 
-6- CAIXA ELETRÔNICO. Escrever um programa que lê um valor inteiro em reais e calcula qual o menor número possível de notas 
-de 100, 50, 20, 10, 5, 2 e 1 em que o valor lido pode ser entregue ao cliente. Escrever a relação de notas necessárias. 
+6- CAIXA ELETRÔNICO. Escrever um programa que lê um valor inteiro em reais e calcula qual o menor número possível de notas
+de 100, 50, 20, 10, 5, 2 e 1 em que o valor lido pode ser entregue ao cliente. Escrever a relação de notas necessárias.
 Utilize um vetor para armazenar o valor de cada tipo de nota. Notas = {100, 50, 20, 10, 5, 2, 1}
 
 */
@@ -11,15 +11,18 @@ Utilize um vetor para armazenar o valor de cada tipo de nota. Notas = {100, 50, 
 #include <stdbool.h>
 
 int main() {
-    
+
     const int notas[10] = {100, 50, 20, 10, 5, 2, 1};
-    int valor_reais;
+    int valor_reais, notas_100, notas_50, notas_20, notas_10, notas_5, notas_2, notas_1;
     _Bool Continuar = 0;
 
     printf("Qual valor voce deseja sacar? ");
     scanf("%d", &valor_reais);
 
-    for (int i = 0; Continuar; i++) {
-        i
+    for (int i = 0; i < 7; i++) {
+        int qntd = valor_reais / notas[i];
+        if (qntd > 0)
+            printf("\nNotas de %d: %d\n", notas[i], qntd);
+        valor_reais %= notas[i];
     }
 }
